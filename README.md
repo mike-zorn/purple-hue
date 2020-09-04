20a4281d596025563d0c29154e2acb85df599cb7
Mike Zorn <apechimp@icloud.com>
2020-09-04 09:13:00 -0700

    WIP purple air

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Change a hue light's color based on air quality data from purple air
 - [ ] Read settings file for user name
 - [ ] Read settings file for light name
 - [ ] set light to a color
-- [ ] Get air quality from purple air via [this](https://github.com/mrsharpoblunto/purple-rain/blob/master/index.js#L103)
+- [x] Get air quality from purple air via [this](https://github.com/mrsharpoblunto/purple-rain/blob/master/index.js#L103)
+- [ ] Convert purple air readings to AQI via [this](https://docs.google.com/document/d/15ijz94dXJ-YAZLi9iZ_RaBwrZ4KtYeCy08goGBwnbCU/edit)
 
 - [ ] create second utility for adding the user name to the settings file
 - [ ] Read correction factors from settings file
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use hueclient::bridge::Bridge;
 use hueclient::HueError;
+use serde::Deserialize;
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
+    let purpleair_response = match get_purple_air_for_sensor(39051).await {
+        Err(why) => panic!("{:?}",why),
+        Ok(purpleair_response) => purpleair_response,
+    };
+    println!("aqi {}", purpleair_response.lrapa_pm25());
+
     let mut bridge = Bridge::discover_required();
     println!("bridge {:#?}", bridge);
     let user = match register_user(&bridge) {
@@ -17,6 +26,16 @@ fn main() {
     println!("lights {:?}", lights);
 }
 
+async fn get_purple_air_for_sensor(sensor_id: u16) -> Result<PurpleairResponse, Box<dyn std::error::Error>> {
+    let url = format!("https://www.purpleair.com/json?show={}", sensor_id);
+    let resp = reqwest::get(&url)
+        .await?
+        .json::<PurpleairResponse>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(resp)
+}
+
 fn register_user(bridge: &Bridge) -> Result<String, HueError> {
     loop {
         let r = bridge.register_user("purple-hue");
@@ -28,5 +47,37 @@ fn register_user(bridge: &Bridge) -> Result<String, HueError> {
             },
             _ => return r,
         }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct PurpleairResponse {
+    results: Vec<PurpleAirResult>,
+}
+
+#[derive(Debug,Deserialize)]
+struct PurpleAirResult {
+    #[serde(alias = "Stats")]
+    stats: String, // JSON encoded JSON stats...might be useful to get 10 minute average (v1 property)
+    #[serde(alias = "PM2_5Value")]
+    pm25_value: String,
+}
+
+impl PurpleairResponse {
+    fn pm25(self) -> f64 {
+        self.results.iter().fold(0.0, |acc, r| match r.pm25() {
+            Ok(pm25) => acc+pm25,
+            Err(_) => acc,
+        }) / (self.results.len() as f64)
+    }
+
+    fn lrapa_pm25(self) -> f64 {
+        0.5*self.pm25()-0.66
+    }
+}
+
+impl PurpleAirResult {
+    fn pm25(&self) -> Result<f64, std::num::ParseFloatError> {
+        self.pm25_value.parse::<f64>()
     }
 }
